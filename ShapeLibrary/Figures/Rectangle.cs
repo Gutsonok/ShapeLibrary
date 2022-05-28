51bd0b51cebd71edd7f9d546fb86c7678788fed4
beljaev <beljaev@action-media.ru>
2022-05-28 12:06:34 +0500

    Add rectangle

---
+++ ShapeLibrary/Figures/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using ShapeLibrary.Abstractions;
+
+namespace ShapeLibrary.Figures;
+
+public class Rectangle : IShape
+{
+    private readonly double _height;
+    
+    private readonly double _width;
+    
+    private readonly double _area;
+    
+    public Rectangle(double height, double width)
+    {
+        if (height <= 0) throw new ArgumentException("The height should be greater than zero");
+        if (width <= 0) throw new ArgumentException("The width should be greater than zero");
+
+        _height = height;
+        _width = width;
+        _area = _height * _width; 
+    }
+
+    public double Area => _area;
+}
